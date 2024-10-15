76365e4a8eeea879e598604e4f5c2dce2ab57357
Ferin <charl@DESKTOP-ODHNF1T>
2024-10-16 04:45:11 +0700

    completed Destruction script

--- Script/Destructible_obj.gd
+++ Script/Destructible_obj.gd
@@ -1,20 +1,28 @@
 extends Node2D
+
 class_name Destructible_obj
-@export var obj_to_destroy:Node2D
-@export var clickable_area:Area2D
 
-var health : int = 10
+@export var obj_to_destroy: Node2D
+@export var clickable_area: Area2D
 
-# Called when the node enters the scene tree for the first time.
-func damage(dmg:int):
-	health = health - 2
-	if health <= 0:
-		queue_free()
-	pass
-	
+@export var health: int = 10
+
 func _ready() -> void:
-	connect("mouse_entered", do_damage)
+	# Connect the input event for the clickable area
+	clickable_area.connect("input_event",_on_clickable_area_input_event)
 
-func do_damage():
-	print(get_node('.'))
-	queue_free()
+func _on_clickable_area_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.pressed and Input.is_action_pressed("left_click"):  # Check for left mouse button
+			do_dmg()
+
+func do_dmg() -> void:
+	if health > 0:  # Check if health is greater than zero
+		health -= 1  # Reduce health by 1
+		print(health)
+	if health <= 0:  # Check if health is zero or less
+		destroy()  # Call destroy to remove the object
+
+func destroy():
+	if obj_to_destroy:
+		queue_free()# Remove the specified child object
